b742e5d58d50c6c961648917a8a5f06efbe94bf6
bengeorge10 <bengeorge567@gmail.com>
2023-06-13 19:19:07 -0400

    first push

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
+//test
 //http://localhost:3000/name
 app.get('/', function (req, res) {
   res.send("<h1>C0861079 - Ben</h1>")
